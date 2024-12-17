1171fc9329e7d7cceabf6ee816989eba653d4b20
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2024-12-17 10:40:07 +0100

    update

---
+++ test.py
@@ -0,0 +1,20 @@
+namn = "Anna"
+ålder = 25
+print(f"hej, vad heter du? {namn}. Hur gammal är du? {ålder} ")
+
+
+x = 23
+y = 12
+z = x+y
+print (z)
+
+# Summan av x och y är:
+x = 10
+y = 20
+summa = x+y
+print (f"{summa}")
+
+x = 42
+y = str(x)
+print(y)
+print(type(y))
