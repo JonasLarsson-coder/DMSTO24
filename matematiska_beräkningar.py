2d29858a30a7c7b893869e5eb6b05f0a8e902c78
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2024-12-19 09:12:13 +0100

    update

---
+++ matematiska_beräkningar.py
@@ -0,0 +1,23 @@
+
+
+x = 10
+y =  3
+
+print(x+y, x-y, x*y, x/y, x//y, x%y)
+
+#konvertera ett flyttal till heltal
+x = 3.14
+print(int(x))
+
+#konvertera ett heltal till ett flyttal
+x = 5
+print(float(x))
+
+x = 0.1
+y = 0.2
+print(round(x+y, 1))
+
+x = input("Skriv första talet: ") 
+y = input("Skriv andra talet: ") 
+summa = int(x) + int(y) 
+print(f"Summan är: {summa}")
