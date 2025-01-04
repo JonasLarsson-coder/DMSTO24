783d79e4605a1196d144f9ccb55c626d71e87ac0
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-04 12:23:43 +0100

    update

---
+++ inlamningsuppgift1/uppgift-1.py
@@ -0,0 +1,16 @@
+
+def is_odd(x: int) -> bool:
+    """
+    Returnerar True om x är udda, annars False.
+    """
+    pass # Ta bort denna rad och skriv din kod här
+
+
+
+
+
+x = 3
+def is_odd(x):
+    return x % 2 != 0
+print(is_odd(x))
+
