dcd16d3535fcc7f956b8d7309965c639869b021d
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-07 11:30:05 +0100

    Update

--- inlamningsuppgift1/inlupp/uppgift-10/uppgift_10.py
+++
@@ -1,8 +0,0 @@
-# Uppgift 10
-# Skapa en funktion celsius_to_fahrenheit(celsius) som konverterar en temperatur från Celsius till Fahrenheit.
-
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
--- inlamningsuppgift1/inlupp/uppgift-6/uppgift_6.py
+++
@@ -1,8 +0,0 @@
-# Uppgift 6
-# Skapa en funktion multiplication_table(n, limit) som returnerar multiplikationstabellen för n upp till limit i en lista.
-
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
