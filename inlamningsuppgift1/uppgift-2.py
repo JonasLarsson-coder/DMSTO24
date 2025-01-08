c4d6ef753c36b72c0fa54f6900642a93c8240bce
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-08 14:32:27 +0100

    update

--- inlamningsuppgift1/uppgift-2.py
+++ inlamningsuppgift1/uppgift-2.py
@@ -1,15 +1,15 @@
 
 # Summera alla tal i en lista.
 
-numberlist = [20, 10, 30, 40]
+numbers = [20, 10, 30, 40]
 
 from typing import List
 
-def sum_list(numberlist: List [int]) -> int:
+def sum_list(numbers: List [int]) -> int:
     """
     Returnerar summan av alla siffror i listan.
     """
-    return sum(numberlist)
-print (sum(numberlist))
+    return sum(numbers)
+print (sum(numbers))
 # Numberlist innehåller 4 olika tal. Sum() adderar alla tal i listan
 
