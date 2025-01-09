eb26d8cda02966cbd0f0c4806d568c34ee109ff5
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-09 11:27:57 +0100

    update

---
+++ inlamningsuppgift1/uppgift-10.py
@@ -0,0 +1,15 @@
+
+# Uppgift 10
+# Skapa en funktion celsius_to_fahrenheit(celsius) som konverterar en temperatur från Celsius till Fahrenheit.
+
+
+def celsius_to_fahrenheit(celsius):
+    # Formeln för att konvertera Celsius till Fahrenheit
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+
+print(celsius_to_fahrenheit(5))   
+print(celsius_to_fahrenheit(2))  
+print(celsius_to_fahrenheit(-3))  
+print(celsius_to_fahrenheit(30))
---
+++ inlamningsuppgift1/uppgift-9.py
@@ -0,0 +1,14 @@
+# Uppgift 9
+# Skapa en funktion is_palindrome(string) som kontrollerar om en given sträng är ett palindrom (dvs. samma framifrån och bakifrån).
+
+
+def is_palindrome(string):
+    # Ta bort mellanslag och gör strängen gemen
+    cleaned_string = ''.join(char.lower() for char in string if char.isalnum())
+    
+    # Kontrollera om strängen är samma framifrån och bakifrån
+    return cleaned_string == cleaned_string[::-1]
+
+
+print(is_palindrome("Anna"))             # True
+print(is_palindrome("Hello"))            # False
