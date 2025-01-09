11bfadefa353f607a5466dc492a376b8265950a9
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-09 11:43:22 +0100

    update

--- DMSTO24-DS-main/inlupp/uppgift-11/uppgift_11.py
+++ DMSTO24-DS-main/inlupp/uppgift-11/uppgift_11.py
@@ -1,8 +1,10 @@
 # Uppgift 11
-# Skapa en funktion word_count(text) som returnerar antalet ord i en given text.
+# Skapa en funktion word_count(text) som returnerar antalet ord i en given text
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
-    """
-    Skriv beskrivning här.
-    """
-    pass # Ta bort denna rad och skriv din kod här
+def word_count(text):
+    # Dela upp texten i ord baserat på mellanslag
+    words = text.split()
+    # Returnera antalet ord
+    return len(words)
+print(word_count("hur många ord är det i den här meningen?"))
+#Returnerar 9, vilket är antal ord i meningen.
