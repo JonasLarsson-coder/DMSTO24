2051ec143b426f7fa870884593e7e5e99113f159
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-09 11:42:56 +0100

    update

---
+++ inlamningsuppgift1/uppgift-11.py
@@ -0,0 +1,10 @@
+# Uppgift 11
+# Skapa en funktion word_count(text) som returnerar antalet ord i en given text
+
+def word_count(text):
+    # Dela upp texten i ord baserat på mellanslag
+    words = text.split()
+    # Returnera antalet ord
+    return len(words)
+print(word_count("hur många ord är det i den här meningen?"))
+#Returnerar 9, vilket är antal ord i meningen.
---
+++ inlamningsuppgift1/uppgift-12.py
@@ -0,0 +1,14 @@
+# Uppgift 12
+# Skapa en funktion create_student_register(students) som tar emot en lista med namn och ålder och returnerar en dictionary där namnet är nyckeln och åldern är värdet.
+
+def create_student_register(students):
+    # Skapa en dictionary där namnet är nyckeln och åldern är värdet
+    student_register = {name: age for name, age in students}
+    return student_register
+
+
+students = [("Jonas", 40), ("Thomas", 45), ("Mary", 32), ("Liam", 20)]
+print(create_student_register(students))
+
+
+"""PASS i PYTEST"""
