41cce372625f55dc3487de919c08b260da647d11
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-09 09:53:22 +0100

    update

--- DMSTO24-DS-main/inlupp/uppgift-4/uppgift_4.py
+++ DMSTO24-DS-main/inlupp/uppgift-4/uppgift_4.py
@@ -1,8 +1,16 @@
 # Uppgift 4
 # Skapa en funktion fibonacci(n) som returnerar en lista med de första n Fibonacci-talen.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def fibonacci(n: int) -> int:
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+print(fibonacci(10))
--- DMSTO24-DS-main/inlupp/uppgift-5/uppgift_5.py
+++ DMSTO24-DS-main/inlupp/uppgift-5/uppgift_5.py
@@ -1,8 +1,16 @@
 # Uppgift 5
 # Skapa en funktion filter_odd(numbers) som returnerar en lista med alla jämna tal från den givna listan.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def filter_odd(numbers: list[int]) -> list[int]:
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    
+def filter_odd(numbers: list[int]) -> list[int]:
+    """
+    Skriv beskrivning här.
+    """
+    return [num for num in numbers if num % 2 == 0]
+
+
+#num for num är en loop som checkar om talet är jämnt (if num % 2 == 0)
