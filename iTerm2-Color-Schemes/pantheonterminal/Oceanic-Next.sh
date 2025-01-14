afd7d99cc1b3471980ea3d7fbcd2ca263fe448c6
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-14 16:51:53 +0100

    update

---
+++ iTerm2-Color-Schemes/pantheonterminal/Oceanic-Next.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dconf load /org/pantheon/terminal/settings/ <<COLORS
+[/]
+name='Oceanic-Next'
+cursor-color='#c1c5cd'
+foreground='#c1c5cd'
+background='rgba(27,43,52,.95)'
+palette='#1b2b34:#db686b:#a2c699:#f2ca73:#7198c8:#bd96c2:#74b1b2:#ffffff:#68737d:#db686b:#a2c699:#f2ca73:#7198c8:#bd96c2:#74b1b2:#ffffff'
+COLORS
