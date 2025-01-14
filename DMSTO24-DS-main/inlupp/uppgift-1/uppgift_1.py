3c2b36472f0ddfa3a437e541d516086c695ede8d
JonasLarsson-coder <jonas.larsson.data24sto@edu.tucsweden.se>
2025-01-14 17:49:43 +0100

    update

--- DMSTO24-DS-main/inlupp/uppgift-1/uppgift_1.py
+++ DMSTO24-DS-main/inlupp/uppgift-1/uppgift_1.py
@@ -1,4 +1,4 @@
-# Uppgift 1
+# Uppgift 1.
 # Skapa en funktion is_odd(x) som returnerar True om x är udda och False om x är jämnt.
 # def funktions_namn(variabel_namn: datatyp) -> returtyp:
 # Exempel: def is_odd(x: int) -> bool:
